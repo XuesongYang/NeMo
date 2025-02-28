e55cb12c69d48346293d49fa4bd380955a6dfd30
Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
2025-02-28 11:46:26 -0800

    modified speaker embedding extraction script to write embed files with the same directory tree as audio filepaths.
    Signed-off-by: Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>

--- scripts/t5tts/speaker_embedding_extraction.py
+++ scripts/t5tts/speaker_embedding_extraction.py
@@ -1,34 +1,40 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
+import argparse
+import os
+from pathlib import Path
+
 import pytorch_lightning as pl
+import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.strategies import DDPStrategy
-import os
-from nemo.collections.asr.parts.preprocessing.segment import AudioSegment
-import argparse
+from torch.utils.data import DataLoader, Dataset
+
 import nemo.collections.asr as nemo_asr
-from nemo.collections.tts.parts.utils.tts_dataset_utils import get_base_dir
-from pathlib import Path
+from nemo.collections.asr.parts.preprocessing.segment import AudioSegment
 from nemo.collections.asr.parts.utils.manifest_utils import read_manifest
 
+
 class AudioDataset(Dataset):
-    def __init__(self, file_list, base_audio_dir, sample_rate=16000):
+    def __init__(self, audio_file_list, base_audio_dir, sample_rate=16000):
         self.sample_rate = sample_rate
         self.combined_file_list = []
-        for file_path in file_list:
+        for file_path in audio_file_list:
             audio_file_path = os.path.join(base_audio_dir, file_path)
-            self.combined_file_list.append({
-                "rel_file_path": file_path,
-                "audio_file_path": audio_file_path,
-            })
-        self.base_data_dir = get_base_dir([item["audio_file_path"] for item in self.combined_file_list])
+            self.combined_file_list.append(
+                {
+                    "rel_file_path": file_path,
+                    "audio_file_path": audio_file_path,
+                }
+            )
 
     def __len__(self):
         return len(self.combined_file_list)
 
     def get_wav_from_filepath(self, file_path):
         features = AudioSegment.segment_from_file(
-            file_path, target_sr=self.sample_rate, n_segments=-1, trim=False,
+            file_path,
+            target_sr=self.sample_rate,
+            n_segments=-1,
+            trim=False,
         )
         audio_samples = features.samples
         audio = torch.tensor(audio_samples)
@@ -37,37 +43,34 @@ class AudioDataset(Dataset):
 
     def __getitem__(self, idx):
         audio_file_path = self.combined_file_list[idx]["audio_file_path"]
-        rel_audio_path = Path(audio_file_path).relative_to(self.base_data_dir).with_suffix("")
-        rel_audio_path_as_text_id = str(rel_audio_path).replace("/", "_")
+        rel_audio_path = self.combined_file_list[idx]["rel_file_path"]
         audio, audio_length = self.get_wav_from_filepath(audio_file_path)
-        
+
         return {
             "audio": audio,
             "audio_length": audio_length,
             "audio_file_path": audio_file_path,
-            "rel_audio_path_as_text_id": rel_audio_path_as_text_id
+            "rel_audio_path": rel_audio_path,
         }
-    
+
     def collate_fn(self, batch):
         audios_padded = []
         audio_lengths = []
         audio_file_paths = []
-        rel_audio_path_as_text_ids = []
+        rel_audio_paths = []
         max_audio_length = max(item["audio_length"].item() for item in batch)
         for item in batch:
-            audio = torch.nn.functional.pad(
-                item["audio"], (0, max_audio_length - item["audio"].size(0)), value=0
-            )
+            audio = torch.nn.functional.pad(item["audio"], (0, max_audio_length - item["audio"].size(0)), value=0)
             audios_padded.append(audio)
             audio_lengths.append(item["audio_length"])
-            rel_audio_path_as_text_ids.append(item["rel_audio_path_as_text_id"])
+            rel_audio_paths.append(item["rel_audio_path"])
             audio_file_paths.append(item["audio_file_path"])
-        
+
         return {
             "audios": torch.stack(audios_padded),
             "audio_lengths": torch.stack(audio_lengths),
             "audio_file_paths": audio_file_paths,
-            "rel_audio_path_as_text_ids": rel_audio_path_as_text_ids
+            "rel_audio_paths": rel_audio_paths,
         }
 
 
@@ -76,7 +79,7 @@ class EmbeddingExtractor(pl.LightningModule):
         super().__init__()
         self.sv_model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained(model_name='titanet_large')
         self.sv_model.eval()
-        self.out_dir = out_dir
+        self.out_dir = Path(out_dir)
 
     def forward(self, batch):
         with torch.no_grad():
@@ -87,11 +90,14 @@ class EmbeddingExtractor(pl.LightningModule):
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         speaker_embeddings = self(batch)
-        for i, file_id in enumerate(batch["rel_audio_path_as_text_ids"]):
-            out_file_path = os.path.join(self.out_dir, file_id + ".pt")
+        for i, rel_path in enumerate(batch["rel_audio_paths"]):
             speaker_embedding = speaker_embeddings[i]
+            output_rel_path = Path(rel_path).with_suffix(".pt")
+            out_file_path = self.out_dir / output_rel_path
+            out_file_path.parent.mkdir(parents=True, exist_ok=True)
             torch.save(speaker_embedding.cpu().type(torch.float32), out_file_path)
         return None
+
 
 if __name__ == "__main__":
     """
@@ -116,13 +122,8 @@ if __name__ == "__main__":
 
     os.makedirs(args.save_dir, exist_ok=True)
     records = read_manifest(args.manifest)
-    unique_audio_files = {}
-    for record in records:
-        unique_audio_files[record["audio_filepath"]] = True
-        if "context_audio_filepath" in record:
-            # Also add contexts, if they exist
-            unique_audio_files[record["context_audio_filepath"]] = True
-    file_list = list(unique_audio_files.keys())
+    records.sort(key=lambda x: x["duration"], reverse=True)
+    file_list = [record["audio_filepath"] for record in records]
 
     trainer = Trainer(
         devices=args.devices,
@@ -133,11 +134,11 @@ if __name__ == "__main__":
         max_epochs=1,
         logger=False,
     )
-    
+
     embedding_extractor = EmbeddingExtractor(args.save_dir)
 
     dataset = AudioDataset(
-        file_list=file_list,
+        audio_file_list=file_list,
         base_audio_dir=args.audio_base_dir,
         sample_rate=16000,
     )
