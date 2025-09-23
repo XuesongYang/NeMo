ade72debaf56b17a25f4aa968b68742a4ad0d7d2
Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
2025-09-23 02:07:58 -0700

    [iclr] added Emilia-YODAS support to extend nemo manifest with context audio script.
    Signed-off-by: Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>

--- scripts/magpietts/extend_nemo_manifest_with_context_audio.py
+++ scripts/magpietts/extend_nemo_manifest_with_context_audio.py
@@ -528,6 +528,8 @@ def _get_parsed_speaker_id_for_dataset(dataset_name_arg, record):
         return _parse_speaker_id_rivaEmmaMeganSeanTom(record)
     elif dataset_name_arg == "jhsdGtc20Amp20Keynote":
         return _parse_speaker_id_jhsdGtc20Amp20Keynote(record)
+    elif dataset_name_arg == "Emilia-YODAS":
+        return _parse_speaker_id_nvyt2505(record)
     else:
         logger.error(
             f"Unsupported dataset_name '{dataset_name_arg}' provided. Please check the --dataset-name argument."
@@ -553,6 +555,7 @@ def main():
             "rivaLindyRodney",
             "rivaEmmaMeganSeanTom",
             "jhsdGtc20Amp20Keynote",
+            "Emilia-YODAS"
         ],
         help="Name of the dataset being processed. This determines the speaker ID parsing logic.",
     )
