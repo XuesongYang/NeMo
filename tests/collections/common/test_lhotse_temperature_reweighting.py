19ce594513b43e0e590feb3fce440940975260bf
Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
2025-12-16 16:43:05 -0800

    Update tests/collections/common/test_lhotse_temperature_reweighting.py
    Signed-off-by: Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>

--- tests/collections/common/test_lhotse_temperature_reweighting.py
+++ tests/collections/common/test_lhotse_temperature_reweighting.py
@@ -18,7 +18,6 @@ Integration tests mock data loading and verify that weights passed to mux() are 
 from unittest.mock import MagicMock, patch
 
 import numpy as np
-import pytest
 from lhotse import CutSet
 from omegaconf import OmegaConf
 
