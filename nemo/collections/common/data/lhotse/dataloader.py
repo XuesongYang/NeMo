48002e0733418e58e27774177361dc52d4a3dddd
Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
2025-12-17 12:01:29 -0800

    bugfix: added reweighting_temperature into default LhotseDataLoadingConfig. Otherwise, it would not be passed to propagate_attrs.
    Signed-off-by: Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>

--- nemo/collections/common/data/lhotse/dataloader.py
+++ nemo/collections/common/data/lhotse/dataloader.py
@@ -108,6 +108,8 @@ class LhotseDataLoadingConfig:
     shard_seed: int | str = "trng"
     max_open_streams: int | None = None
     cuda_expandable_segments: bool = True
+    # Temperature for re-weighting datasets. 1 is a neutral value. Lower temperature over-samples smaller datasets, and vice versa.
+    reweight_temperature: List[float] | None = None
     # e. Multi-config related options.
     #    Setting multi_config=True will scan the config for keys with DictConfig values,
     #    create a separate sampler for each, and fuse the samplers according to sampler_fusion.
